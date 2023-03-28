273cf44bb79e3c53cb60efab51a9005c2fe7953e
Juho Simojoki <juho.simojoki@metropolia.fi>
2023-03-28 12:48:50 +0300

    Another e2e test

--- cypress/e2e/pokedex.cy.js
+++ cypress/e2e/pokedex.cy.js
@@ -1,8 +1,16 @@
-/* eslint-disable no-undef */
 describe('Pokedex', function() {
-  it('front page can be opened', function() {
+  beforeEach( function() {
     cy.visit('http://localhost:5000')
+  })
+
+  it('front page can be opened', function() {
     cy.contains('ivysaur')
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
+  })
+
+  it('single pokemen page can be opened', function() {
+    cy.contains('ivysaur').click()
+    cy.contains('overgrow')
+    cy.contains('chlorophyll')
   })
 })
